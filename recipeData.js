1d3e019da691a4214a8d7b291ca372fc6784f7dd
LordSir101 <49959269+LordSir101@users.noreply.github.com>
2019-11-10 13:18:11 -0500

    Add files via upload

---
+++ recipeData.js
@@ -0,0 +1,96 @@
+
+
+let api = "https://api.edamam.com/search?q="
+let id = "&app_id=$590fe5b0&app_key=$675b5a02077391477744369154977f18"
+let entry;
+
+let output = document.getElementById('output');
+let foodChoice = document.getElementById('foodChoice');
+let calorieMin = document.getElementById('calorieMin');
+let calorieMax= document.getElementById('calorieMax');
+let submitButton = document.getElementById('submit');
+
+var urls = new Array();
+var titles= new Array();
+
+submitButton.addEventListener('click', getURL);
+
+function getURL(e){
+  e.preventDefault();
+  entry = foodChoice.value;
+  let url= api + entry+ id+ "";
+  display(url);
+}
+
+function display(url){
+  fetch(url)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+
+      console.log(data);
+      // Work with JSON data here
+      getRecipe(data);
+      getTitles(data);
+
+      for(recipe = 0; recipe < urls.length; recipe++){
+
+        //var link = document.createElement('a');
+        var url = urls[recipe];
+
+        // Create the text node for anchor element.
+        var link = document.createElement('a');
+        var title = document.createTextNode(titles[recipe] + "");
+
+        // Append the text node to anchor element.
+        link.appendChild(title);
+
+        // Set the href property.
+        link.href = url + "";
+
+        // Append the anchor element to the body.
+        var li = document.createElement('li');
+        li.appendChild(link);
+        output.appendChild(li);
+      }
+
+
+    })
+    .catch(err => {
+      // Do something for an error here
+    })
+}
+
+function getRecipe(data){
+
+  var minCal = calorieMin.value;
+  var maxCal = calorieMax.value;
+  var index = 0
+  //console.log(data.hits[1].recipe.url);
+  for(recipe = 0; recipe < data.hits.length; recipe++){
+
+    if (data.hits[recipe].recipe.calories >= minCal && data.hits[recipe].recipe.calories <= maxCal){
+      urls[index] = data.hits[recipe].recipe.url;
+      index++;
+    }
+  }
+
+  return;
+}
+
+function getTitles(data){
+
+  var minCal = calorieMin.value;
+  var maxCal = calorieMax.value;
+
+  var index = 0;
+  for(recipe = 0; recipe < data.hits.length; recipe++){
+    if (data.hits[recipe].recipe.calories >= minCal && data.hits[recipe].recipe.calories <= maxCal){
+      titles[index] = data.hits[recipe].recipe.label;
+      index++;
+    }
+  }
+
+  return;
+}
